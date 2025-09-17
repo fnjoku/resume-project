bfccec54038f816335c7b7c1f25e84e3beddd514
Favour Njoku <fnjoku.university@gmail.com>
2025-09-17 09:08:15 -0500

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,16 @@
+// Define Variables
+let my_name = "Favour";
+let my_email = "njo205623@oru.edu"
+let title = "Student";
+let experience = "N/A";
+let education = "Oral Roberts University";
+let skills = ["eating", "programming", "slepping", "music"];
+//let skills = "something"; //TODO
+
+// Connect Variables to the HTML (ligaments)
+document.getElementById("my_name").textContent = my_name
+document.getElementById("my_email").textContent = my_email
+document.getElementById("title").textContent = title
+document.getElementById("experience").textContent = experience
+document.getElementById("education").textContent = education
+document.getElementById("skills").textContent = skills
